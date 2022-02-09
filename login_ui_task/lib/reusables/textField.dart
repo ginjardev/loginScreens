1d62b2874ba85960c2ec3a198e533895d306c6c9
Iniubong Arthur <arthurshur@gmail.com>
2022-02-09 01:49:52 +0100

    other updates

--- login_ui_task/lib/reusables/textField.dart
+++ login_ui_task/lib/reusables/textField.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  final Widget child;
+  Widget? child;
   final String text;
 
-  MyTextField({required this.text, required this.child});
+  MyTextField({required this.text, this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: TextField(
-        
+        style: TextStyle(fontWeight: FontWeight.bold),
         decoration: InputDecoration(
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(35),)),
+              borderRadius: BorderRadius.all(
+            Radius.circular(50)
+          ),
+          borderSide: BorderSide.none,
+          ),
           suffixIcon: child,
-          suffixIconColor: Color.fromARGB(255, 15, 75, 13),
+          suffixIconColor: Color.fromARGB(255, 34, 180, 29),
           hintText: text,
           hintStyle: TextStyle(
             color: Color.fromARGB(255, 48, 139, 45),
-            ),
-          fillColor: Color.fromARGB(255, 218, 213, 207), 
+          ),
+          fillColor: Color.fromARGB(255, 236,242,240),
           filled: true,
         ),
-        ),
+      ),
     );
   }
 }
