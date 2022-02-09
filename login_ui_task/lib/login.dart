24652918603461245cf9af07324936281b429efc
Iniubong Arthur <arthurshur@gmail.com>
2022-02-09 04:05:40 +0100

    updated login

--- login_ui_task/lib/login.dart
+++ login_ui_task/lib/login.dart
@@ -13,8 +13,10 @@ class LoginPage extends StatefulWidget {
 enum Status { Remember }
 
 class _LoginPageState extends State<LoginPage> {
-  
   Status? _character = Status.Remember;
+  int id = 1;
+  String radioButtonItem = 'ONE';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,17 +30,32 @@ class _LoginPageState extends State<LoginPage> {
         )),
         child: Stack(
           children: [
+            Positioned(
+              left: MediaQuery.of(context).size.width * 0.39,
+              top: 120,
+              child: Container(
+              alignment: Alignment.center,
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "images/butter.png",
+                      ),
+                      )
+                      ),
+            ),
+              ),
             Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
+                // SizedBox(height: 40,),
                 Container(
                   alignment: Alignment.bottomCenter,
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(30)
-                          )
-                          ),
+                          BorderRadius.only(topLeft: Radius.circular(30))),
                   child: Column(
                     children: [
                       Container(
@@ -75,6 +92,63 @@ class _LoginPageState extends State<LoginPage> {
                           child: Icon(Icons.visibility_off),
                         ),
                       ),
+
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Row(
+                          children: [
+                            Radio(
+                                value: 1,
+                                activeColor: Color.fromARGB(255, 46, 112, 80),
+                                groupValue: id,
+                                onChanged: (index) {
+                                  setState(() {
+                                    radioButtonItem = 'ONE';
+                                    id = 1;
+                                  });
+                                }),
+                            Expanded(child: Text('Remember me')),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Forgot password?",
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 48, 139, 45),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 80,
+                      ),
+                      Button(
+                          child: Text("Login",
+                          style: TextStyle(color: Colors.white),),
+                          onPressed: () {},
+                          color: Color.fromARGB(255, 0, 106, 66)),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            Text(
+                              "Sign up",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 48, 139, 45),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ]),
+                      SizedBox(
+                        height: 20,
+                      )
                     ],
                   ),
                 ),
