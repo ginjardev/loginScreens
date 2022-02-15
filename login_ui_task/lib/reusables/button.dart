60c5397a05c85105819efb793df48a2318366eaf
Iniubong Arthur <arthurshur@gmail.com>
2022-02-15 01:54:45 +0000

    refactored reusable button

--- login_ui_task/lib/reusables/button.dart
+++ login_ui_task/lib/reusables/button.dart
@@ -2,31 +2,31 @@ import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
   final Widget child;
-  final Function onPressed;
+  //final Function onPressed;
+  final String routeName;
   Color color = Colors.greenAccent;
-
 
   Button({
     required this.child,
-    required this.onPressed,
+    //required this.onPressed,
     required this.color,
+    required this.routeName,
   });
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: (
-        MaterialButton(
-        onPressed: () {},
+      child: (MaterialButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(routeName);
+        },
         child: child,
         color: color,
         minWidth: MediaQuery.of(context).size.width * 0.90,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(20),
-            )
-        ),
-        
+            borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        )),
       )),
     );
   }
